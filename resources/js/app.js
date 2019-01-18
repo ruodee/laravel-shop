da191b41bff7032c7a9fd04787bc72c75f159b02
vagrant <vagrant@homestead>
2019-01-18 15:55:47 +0000

    a half 做完vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+require('./components/SelectDistrict');
 
 /**
  * The following block of code may be used to automatically register your
---
+++ resources/views/user_addresses/create_and_edit.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+@section('title', '新增收货地址')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-10 offset-lg-1">
+            <div class="card">
+                <div class="card-header">
+                    <h2 class="text-center">新增收货地址</h2>
+                </div>
+                <div class="card-body">
+                    <from class="form-horizontal" role="form">
+                       <!-- inline-template 代表通过内联方式引入组件  -->
+                       <select-district inline-template>
+                           <div class="form-row">
+                               <label for="" class="col-form-lable col-sm-2 text-md-right">省市区</label>
+                               <div class="col-sm-3">
+                                   <secect class="formcontrol" v-model="provinceId">
+                                       <option value="">选择省</option>
+                                       <option v-for="(name,id) in provinces" :value="id">@{{ name }}</option>
+                                   </secect>
+                               </div>
+                               <div class="col-sm-3">
+                                    <select name="" id="" class="form-control" v-model="cityId">
+                                           <option value="">选择市</option>
+                                           <option v-for="(name,id) in cities" :value="id">@{{ name }}</option>
+                                    </select>
+                               </div>
+                               <div class="col-sm-3">
+                                   <select name="" id="" class="form-control" v-model="districtId">
+                                       <option value="">选择区</option>
+                                       <option value="" v-for="(name,id) in districts" :value="id">@{{ name }}</option>
+                                   </select>
+                               </div>
+                           </div>
+                       </select-district>
+                    </from>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
