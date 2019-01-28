a7bf364f9a00c1643924a87a2678d7a6c54602ab
ruodee <ruodee@126.com>
2019-01-28 13:16:32 +0000

    异常处理

---
+++ app/Exceptions/InternalException.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Http\Request;
+
+class InternalException extends Exception
+{
+    protected $msgForUser;
+
+    public function __construct(string $message, string $msgForUser = '系统内部错误', int $code = 500)
+    {
+        parent::__construct(string $message, string $msgForUser = '系统内部错误', int $code = 500)
+        $this->msgForUser = $msgForUser;
+    }
+
+    public function render(Request $request)
+    {
+        if ($request->expectJson()) {
+            return response()->json(['msg' => $this->msgForUser], $this->code);
+        }
+        return view('pages.error', ['msg' => $this->msgForUser]);
+    }
+}
