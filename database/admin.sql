04ac7601bbe21bdc590b3c869a998da60e8d18a4
vagrant <ruodee@126.com>
2019-02-11 09:59:10 +0000

    完善假数据填充，管理后台 SQL 文件

--- database/admin.sql
+++ database/admin.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.19, for Linux (x86_64)
 --
--- Host: localhost    Database: laravel-shop-57
+-- Host: localhost    Database: laravel-shop
 -- ------------------------------------------------------
--- Server version   5.7.21-0ubuntu0.16.04.1
+-- Server version	5.7.19-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -103,4 +103,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-12-23  3:01:33
+-- Dump completed on 2019-02-11  9:05:28
