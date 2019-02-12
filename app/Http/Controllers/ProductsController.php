d4be8975d8acb368a275287da70513665b4c0e59
ruodee <ruodee@126.com>
2019-02-12 14:30:54 +0000

    前台商品列表支持按类目筛选

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\OrderItem;
 use Illuminate\Http\Request;
 use App\Models\Product;
@@ -27,19 +28,34 @@ class ProductsController extends Controller
                     });
             });
         }
-            // 是否有提交 order 参数，如果有就赋值给 $order 变量
-            // order 参数用来控制商品的排序规则
-            if ($order = $request->input('order', '')) {
-                // 是否是以 _asc 或者 _desc 结尾
-                if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
-                    // 如果字符串的开头是这 3 个字符串之一，说明是一个合法的排序值
-                    if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
-                        // 根据传入的排序值来构造排序参数
-                        $builder->orderBy($m[1], $m[2]);
-                    }
-
-                }
+        // 如果有传入 category_id 字段，并且在数据库中有对应的类目
+        if ($request->input('category_id') && $category = Category::find($request->input('category_id'))) {
+            // 如果这是一个父类目
+            if ($category->is_directory) {
+                // 则筛选出该父类目下所有子类目的商品
+                $builder->whereHas('category', function ($query) use ($category) {
+                    // 这里的逻辑参考本章第一节
+                    $query->where('path', 'like', $category->path.$category->id.'%');
+                });
+            } else {
+                // 如果这不是一个父类目，则直接筛选此类目下的商品
+                $builder->where('category_id', $category->id);
             }
+        }
+
+        // 是否有提交 order 参数，如果有就赋值给 $order 变量
+        // order 参数用来控制商品的排序规则
+        if ($order = $request->input('order', '')) {
+            // 是否是以 _asc 或者 _desc 结尾
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                // 如果字符串的开头是这 3 个字符串之一，说明是一个合法的排序值
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    // 根据传入的排序值来构造排序参数
+                    $builder->orderBy($m[1], $m[2]);
+                }
+
+            }
+        }
 
         $products = $builder->paginate(16);
         return view('products.index', [
@@ -47,7 +63,9 @@ class ProductsController extends Controller
             'filters'  => [
                 'search' => $search,
                 'order'  => $order,
-            ]
+            ],
+            // 等价于 isset($category) ? $category : null
+            'category'  => $category ?? null,
              ]);
     }
     public function show(Product $product, Request $request)
