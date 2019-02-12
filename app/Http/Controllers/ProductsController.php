6b905fbe63d5482beb92aafa910d932b8c86f3f0
ruodee <ruodee@126.com>
2019-02-12 16:42:32 +0000

    a half of 无限级菜单

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\CategoryService;
 use App\Models\Category;
 use App\Models\OrderItem;
 use Illuminate\Http\Request;
@@ -10,7 +11,7 @@ use App\Exceptions\InvalidRequestException;
 
 class ProductsController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request, CategoryService $categoryService)
     {
         // 创建一个查询构造器
         $builder = Product::query()->where('on_sale', true);
@@ -66,6 +67,8 @@ class ProductsController extends Controller
             ],
             // 等价于 isset($category) ? $category : null
             'category'  => $category ?? null,
+            // 将类目树传递给模板文件
+            'categoryTree' => $categoryService->getCategoryTree(),
              ]);
     }
     public function show(Product $product, Request $request)
