3a9298230e822b5610fb57ed9c424d25f2e24dbb
ruodee <ruodee@126.com>
2019-02-19 14:38:13 +0000

    众筹结束逻辑

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Admin\Controllers;
 
+use App\Services\OrderService;
 use App\Models\CrowdfundingProduct;
 use App\Exceptions\InternalException;
 use App\Http\Requests\Admin\HandleRefundRequest;
@@ -108,7 +109,7 @@ class OrdersController extends Controller
         return redirect()->back();
     }
 
-    public function handleRefund(Order $order, HandleRefundRequest $request)
+    public function handleRefund(Order $order, HandleRefundRequest $request, OrderService $orderService)
     {
         // 判断订单状态是否正确
         if ($order->refund_status !== Order::REFUND_STATUS_APPLIED) {
@@ -123,7 +124,7 @@ class OrdersController extends Controller
                 'extra'     => $extra,
             ]);
             // 调用退款逻辑
-            $this->_refundOrder($order);
+            $orderService->refundOrder($order);
         } else {
             // 将拒绝退款理由放到订单的 extra 字段中
             $extra = $order->extra ?: [];
@@ -136,48 +137,5 @@ class OrdersController extends Controller
         }
 
         return $order;
-    }
-
-    public function _refundOrder($order)
-    {
-        // 判断该订单的支付方式
-        switch ($order->payment_method) {
-            case 'wechat':
-                // 微信退款先留空
-                // todo
-                break;
-            case 'alipay':
-                // 用我们刚刚写的方法生成一个退款订单号
-                $refundNo = Order::getAvailableRefundNo();
-                // 调用支付宝支付实例的 refund 方法
-                $ret = app('alipay')->refund([
-                    'out_trade_no' => $order->no, // 之前的订单流水号
-                    'refund_amount' => $order->total_amount, // 退款金额，单位元
-                    'out_request_no' => $refundNo, // 退款订单号
-                ]);
-                // 根据支付宝的文档，如果返回值里面有 sub_code 字段说明退款失败
-                if ($ret->sub_code) {
-                    // 将退款失败的信息保存存入 extra 字段
-                    $extra = $order->extra;
-                    $extra['refund_faild_code'] = $ret->sub_code;
-                    // 将订单的退款状态标记为退款失败
-                    $order->update([
-                        'refund_no'     => $refundNo,
-                        'refund_status' => Order::REFUND_STATUS_FAILED,
-                        'extra'         => $extra,
-                    ]);
-                } else {
-                    $order->update([
-                        'refund_no'         => $refundNo,
-                        'refund_status'     => Order::REFUND_STATUS_SUCCESS,
-                    ]);
-                }
-
-                break;
-            default:
-                // 原则上不可能出现，这个只是为了代码健壮性
-                throw new InternalException('未知订单支付方式：'.$order->payment_method);
-                break;
-        }
     }
 }
---
+++ app/Console/Commands/Cron/FinishCrowdfunding.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Console\Commands\Cron;
+
+use App\Jobs\RefundCrowdfundingOrders;
+use App\Services\OrderService;
+use App\Models\CrowdfundingProduct;
+use App\Models\Order;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class FinishCrowdfunding extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cron:finish-crowdfunding';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '结束众筹';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        CrowdfundingProduct::query()
+            // 众筹结束时间早于当前时间
+            ->where('end_at', '<=', Carbon::now())
+            // 众筹状态为众筹中
+            ->where('status', CrowdfundingProduct::STATUS_FUNDING)
+            ->get()
+            ->each(function (CrowdfundingProduct $crowdfunding) {
+                // 如果众筹目标金额大于实际众筹金额
+                if ($crowdfunding->target_amount > $crwodfunding->total_amount) {
+                    // 调用众筹失败逻辑
+                    $this->crowdfundingFailed($crowdfunding);
+                } else {
+                    // 否则调用众筹成功逻辑
+                    $this->crowdfundingSucceed($crowdfunding);
+                }
+            });
+    }
+
+    protected function crowdfundingFailed(CrowdfundingProduct $crowdfunding)
+    {
+        // 将众筹状态改为众筹失败
+        $crowdfunding->update([
+            'status'        => CrowdfundingProduct::STATUS_FAIL,
+        ]);
+        dispatch(new RefundCrowdfundingOrders($crowdfunding));
+        // $orderService = app(OrderService::class);
+        // // 查询出所有参与了此众筹的订单
+        // Order::query()
+        //     // 订单类型为众筹商品订单
+        //     ->where('type', Order::TYPE_CROWDFUNDING)
+        //     // 已支付的订单
+        //     ->whereNotNull('paid_at')
+        //     ->whereHas('items', function ($query) use ($crowdfunding) {
+        //         // 包含了当前商品
+        //         $query->where('product_id', $crowdfunding->product_id);
+        //     })
+        //     ->get()
+        //     ->each(function (Order $order) {
+        //         // todo 调用退款逻辑
+        //         $orderService->refundOrder($order);
+        //     });
+    }
+}
