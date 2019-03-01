a8e3b1f1d0e0225a054f9c2da2aafca2e5feec49
ruodee <ruodee@126.com>
2019-03-01 15:05:41 +0000

    一半到实现具体退款落实,函数

--- app/Jobs/RefundInstallmentOrder.php
+++ app/Jobs/RefundInstallmentOrder.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Exceptions\InternalException;
 use App\Models\Installment;
 use App\Models\InstallmentItem;
 use App\Models\Order;
@@ -22,9 +23,9 @@ class RefundInstallmentOrder implements ShouldQueue
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Order $order)
     {
-        //
+        $this->order = $order;
     }
 
     /**
@@ -34,6 +35,59 @@ class RefundInstallmentOrder implements ShouldQueue
      */
     public function handle()
     {
-        //
+        // 如果商品订单支付方式不是分期付款、订单未支付、订单退款状态不是退款中，则不执行后面的逻辑
+        if ($this->order->payment_method !== 'installment'
+            || !$this->order->paid_at
+            || $this->order->refund_status !== Order::REFUND_PROCESSING) {
+            return;
+        }
+        // 找不到对应的分期付款，原则上不可能出现这种情况，这里的判断只是增加代码健壮性
+        if (!$installment = Installment::query()->where('order_id', $this->order->id)->first()) {
+            return;
+        }
+        // 遍历对应分期付款的所有还款计划
+        foreach ($installment->items as $item) {
+            // 如果还款计划未支付，或者退款状态为退款成功或退款中，则跳过
+            if (!$item->paid_at || in_array($item->refund_status, [
+                InstallmentItem::REFUND_STATUS_SUCCESS,
+                InstallmentItem::REFUND_STATUS_PROCESSING,
+            ])) {
+                continue;
+            }
+            // 调用具体的退款逻辑
+            try {
+                $this->refundInstallmentItem($item);
+            } catch (\Exception $e) {
+                \Log::warning('分期退款失败: '.$e->getMessage(), [
+                    'installment_item_id' => $item->id,
+                ]);
+                // 加入某个还款计划退款包错了，则暂时跳过，继续处理下一个还款计划的退款
+                continue;
+            }
+        }
+        // 设定一个全部退款成功的标志位
+        $allSuccess = true;
+        // 再次遍历所有还款计划
+        foreach ($istallment->items as $item) {
+            // 如果该环境计划已经还款，但退款状态不是成功
+            if ($item->paid_at &&
+                $item->refund_status !== InstallmentItem::REFUND_STATUS_SUCCESS) {
+                // 则将标志记为 false
+                $allSuccess = false;
+                break;
+            }
+        }
+        // 如果所有退款都成功，则将对应商品订单的退款状态修改为退款成功
+        if ($allSuccess) {
+            $this->order->update([
+                'refund_status' => Order::REFUND_STATUS_SUCCESS,
+            ]);
+        }
+    }
+
+    protected function refundInstallmentItem(InstallmentItem $item)
+    {
+        // 退款单号使用商品订单的退款号与当前还款计划的序号拼接而成
+        $refundNo = $this->order->refund_no.'_'.$item->sequence;
     }
 }
