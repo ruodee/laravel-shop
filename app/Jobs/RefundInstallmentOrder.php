de3e627762f3f91a6e761fffba3e3c6149634047
vagrant <ruodee@126.com>
2019-03-01 09:07:04 +0000

    refundinstallmentorder jobs

---
+++ app/Jobs/RefundInstallmentOrder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Installment;
+use App\Models\InstallmentItem;
+use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+// ShouldQueue 代表这是一个异步任务
+class RefundInstallmentOrder implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $order;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
