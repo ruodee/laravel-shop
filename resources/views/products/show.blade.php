e211211f5197e32068b5802365eef4dcc9bac49b
ruodee <ruodee@126.com>
2019-03-07 14:48:40 +0000

    商品属性模型

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -107,7 +107,20 @@
                     </ul>
                     <div class="tab-content">
                         <div class="tab-pane active" role="tabpanel" id="product-detail-tab">
-                            {!! $product->description !!}
+                            <!-- 产品属性开始 -->
+                            <div class="properties-list">
+                                <div class="properties-list-title">产品参数：</div>
+                                <ul class="properties-list-body">
+                                    @foreach($product->grouped_properties as $name => $values)
+                                        <li>{{ $name }}: {{ join(' ', $values) }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <!-- 产品属性结束 -->
+                            <!-- 在商品描述外面包了一层div -->
+                            <div class="product-description">
+                                {!! $product->description !!}
+                            </div>
                         </div>
                         <div class="tab-pane" role="tabpanel" id="product-reviews-tab">
                         <!-- 评论列表开始 -->
